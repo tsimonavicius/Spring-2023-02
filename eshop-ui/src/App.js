478d9349decd6f9170f57d74a80c8b0bdab380e4
Tadas Simonavičius <tadas@inventi.lt>
2023-02-02 18:30:19 +0200

    2: create first React component

--- eshop-ui/src/App.js
+++ eshop-ui/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import SayMyName from './components/SayMyName'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <SayMyName />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ eshop-ui/src/components/SayMyName.js
@@ -0,0 +1,5 @@
+function SayMyName() {
+    return <h1>Hello, World!!!</h1>
+}
+
+export default SayMyName
