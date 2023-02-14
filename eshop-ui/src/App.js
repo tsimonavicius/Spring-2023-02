fe2fcf8100ae107e372aa82ce432719bd9bfa908
almantas <almantas.tvarijonavicius@codeacademylt.onmicrosoft.com>
2023-02-14 22:08:44 +0200

    7: added material-ui main app sketch

--- eshop-ui/src/App.js
+++ eshop-ui/src/App.js
@@ -1,42 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import SayMyName from './components/SayMyName'
-import SayMyNameWithState from "./components/SayMyNameWithState";
-import ButtonWithText from "./components/ButtonWithText";
-import SayMyNameUncontrolled from "./components/SayMyNameUncontrolled";
-import SayMyNameWithStateWithHooks from "./components/SayMyNameWithStateWithHooks";
-import ButtonWithTextWithHooks from "./components/ButtonWithTextWithHooks";
-import ProductForm from "./components/ProductForm";
+import AppDashboard from "./components/AppDashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <SayMyName name="Tadas" />
-        <hr/>
-        <SayMyNameWithState/>
-        <hr/>
-        <ButtonWithText text="aaa"/>
-        <hr/>
-        <SayMyNameUncontrolled name="Vardenis"/>
-        <hr/>
-        <SayMyNameWithStateWithHooks/>
-        <hr/>
-        <ButtonWithTextWithHooks text="Paspausk"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ProductForm/>
-      </header>
+      <AppDashboard />
     </div>
   );
 }
