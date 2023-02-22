cf640049fa6150bbce103a05f4af6b2c6c5c22d0
Tadas Simonavičius <tadas@inventi.lt>
2023-02-22 18:42:56 +0200

    11: add id to hard-coded products

--- eshop-ui/src/pages/Products.js
+++ eshop-ui/src/pages/Products.js
@@ -2,8 +2,9 @@ import * as React from "react";
 import {Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
 import AddItemModal from "../components/dashboard/AddItemModal";
 
-function createData(name, description, price) {
+function createData(id, name, description, price) {
     return {
+        id: id,
         name: name,
         description: description,
         price: price,
@@ -12,20 +13,20 @@ function createData(name, description, price) {
 }
 
 const allProducts_dummyData = [
-    createData("labai geras produktas 1", "best desc ever 1", 11.10),
-    createData("labai geras produktas 2", "best desc ever 2", 12.21),
-    createData("labai geras produktas 3", "best desc ever 3", 33.11),
-    createData("labai geras produktas 4", "best desc ever 4", 44.01),
-    createData("labai geras produktas 5", "best desc ever 5", 51.23),
-    createData("labai geras produktas 6", "best desc ever 6", 165.12),
-    createData("labai geras produktas 7", "best desc ever 7", 3456.23),
-    createData("labai geras produktas 8", "best desc ever 8", 66.65),
-    createData("labai geras produktas 8", "best desc ever 8", 66.65),
-    createData("labai geras produktas 8", "best desc ever 8", 66.65),
-    createData("labai geras produktas 8", "best desc ever 8", 66.65),
-    createData("labai geras produktas 8", "best desc ever 8", 66.65),
-    createData("labai geras produktas 8", "best desc ever 8", 66.65),
-    createData("labai geras produktas 8", "best desc ever 8", 66.65),
+    createData(1, "labai geras produktas 1", "best desc ever 1", 11.10),
+    createData(2, "labai geras produktas 2", "best desc ever 2", 12.21),
+    createData(3, "labai geras produktas 3", "best desc ever 3", 33.11),
+    createData(4, "labai geras produktas 4", "best desc ever 4", 44.01),
+    createData(5, "labai geras produktas 5", "best desc ever 5", 51.23),
+    createData(6, "labai geras produktas 6", "best desc ever 6", 165.12),
+    createData(7, "labai geras produktas 7", "best desc ever 7", 3456.23),
+    createData(8, "labai geras produktas 8", "best desc ever 8", 66.65),
+    createData(9, "labai geras produktas 8", "best desc ever 8", 66.65),
+    createData(10, "labai geras produktas 8", "best desc ever 8", 66.65),
+    createData(11, "labai geras produktas 8", "best desc ever 8", 66.65),
+    createData(12, "labai geras produktas 8", "best desc ever 8", 66.65),
+    createData(13, "labai geras produktas 8", "best desc ever 8", 66.65),
+    createData(14, "labai geras produktas 8", "best desc ever 8", 66.65),
 ]
 
 const Products = () => {
