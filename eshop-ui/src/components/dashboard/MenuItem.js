32ef3b8f7d1cb45bb481aa773f01f5c93c88f57d
Tadas Simonavičius <tadas@inventi.lt>
2023-02-22 18:28:06 +0200

    11: mark active menu item

--- eshop-ui/src/components/dashboard/MenuItem.js
+++ eshop-ui/src/components/dashboard/MenuItem.js
@@ -2,15 +2,17 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemButton from "@mui/material/ListItemButton";
 import * as React from "react";
-import {useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import PropTypes from 'prop-types';
 
 const MenuItem = ({label, icon, link}) => {
 
     const navigate = useNavigate();
+    const location = useLocation()
 
     return (
-        <ListItemButton onClick={() => navigate(link)}>
+        <ListItemButton selected={location.pathname === link}
+                        onClick={() => navigate(link)}>
             <ListItemIcon>
                 {icon}
             </ListItemIcon>
