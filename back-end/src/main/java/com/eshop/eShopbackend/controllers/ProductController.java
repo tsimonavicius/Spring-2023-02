c9aabbffa434f4ba05c44a1a4e1cb2dd4c63753e
Tadas Simonavičius <tadas@inventi.lt>
2023-02-22 21:59:43 +0200

    11: enable CORS for products endpoints

--- back-end/src/main/java/com/eshop/eShopbackend/controllers/ProductController.java
+++ back-end/src/main/java/com/eshop/eShopbackend/controllers/ProductController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/products")
+@CrossOrigin("*")
 @Api(tags = {"this is a products controller", "with string array description"})
 public class ProductController {
 
