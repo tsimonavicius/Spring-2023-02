93ca8b9bf6f4d19d0921924bb45d958da5f2fd39
Tadas Simonavičius <tadas@inventi.lt>
2023-02-23 18:50:32 +0200

    12: add react proxy to call backend

--- back-end/src/main/java/com/eshop/eShopbackend/controllers/ProductController.java
+++ back-end/src/main/java/com/eshop/eShopbackend/controllers/ProductController.java
@@ -11,7 +11,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/products")
-@CrossOrigin("*")
 @Api(tags = {"this is a products controller", "with string array description"})
 public class ProductController {
 
--- eshop-ui/src/api/index.js
+++ eshop-ui/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const HTTP = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: '/'
 })
 
 export default HTTP
