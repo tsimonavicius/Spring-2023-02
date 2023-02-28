02c7df95d7296470bd46ff4367efc68f2d38e9ca
almantas <almantas.tvarijonavicius@codeacademylt.onmicrosoft.com>
2023-02-28 20:54:47 +0200

    14: upload file functionality in modal

--- eshop-ui/src/components/dashboard/AddItemModal.js
+++ eshop-ui/src/components/dashboard/AddItemModal.js
@@ -1,68 +1,71 @@
 import * as React from "react";
-import {Button, Dialog, DialogTitle, DialogContent, TextField, DialogContentText, DialogActions } from "@mui/material";
-
+import { Button, Dialog, DialogTitle, DialogContent, TextField, DialogContentText, DialogActions, CardActionArea, CardMedia } from "@mui/material";
+import { createProduct } from "../../api/productsApi";
 
 const AddItemModal = () => {
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [name, setName] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [price, setPrice] = React.useState("");
+  const [imagePreview, setImagePreview] = React.useState(null);
+  const [imageData, setImageData] = React.useState(null);
 
-    const handleClick = () => {
-        setOpen(!open);
-    };
+  const PLACE_HOLDER_IMG = "https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png";
 
-    const addNewProduct = () => {
-        console.log("added something")
-        handleClick();
-    }
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
-    return (
-        <>
-            <Dialog open={open} onClose={handleClick}>
-                <DialogTitle>Add new item</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        To add to this item data, please click 'Add' button
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Item name"
-                        type="text"
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="description"
-                        label="Item description"
-                        type="text"
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="description"
-                        label="Item price"
-                        type="number"
-                        fullWidth
-                        variant="standard"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClick}>Cancel</Button>
-                    <Button onClick={addNewProduct}>Add</Button>
-                </DialogActions>
-            </Dialog>
-            <div style={{marginTop: "10px", textAlign: "center"}}>
-                <Button variant="outlined" onClick={handleClick}>
-                    Add new item
-                </Button>
-            </div>
-        </>
-    )
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    setImageData(file);
+    setImagePreview(URL.createObjectURL(file));
+  };
 
+  const addNewProduct = () => {
+    console.log("added something");
+    createProduct({
+      name: name,
+      description: description,
+      price: price,
+    });
+    handleClick();
+    window.location.reload(false);
+  };
+
+  return (
+    <>
+      <Dialog open={open} onClose={handleClick}>
+        <DialogTitle>Add new item</DialogTitle>
+        <DialogContent>
+          <DialogContentText>To add to this item data, please click 'Add' button</DialogContentText>
+          <TextField autoFocus margin="dense" id="name" label="Item name" type="text" fullWidth variant="standard" value={name} onChange={(event) => setName(event.target.value)} />
+          <TextField autoFocus margin="dense" id="description" label="Item description" type="text" fullWidth variant="standard" value={description} onChange={(event) => setDescription(event.target.value)} />
+          <TextField autoFocus margin="dense" id="price" label="Item price" type="number" fullWidth variant="standard" value={price} onChange={(event) => setPrice(event.target.value)} />
+          <CardActionArea sx={{ maxWidth: "300px", maxHeight: "300px", marginLeft: "22%" }}>
+            <CardMedia component="img" image={imagePreview !== null ? imagePreview : PLACE_HOLDER_IMG} />
+          </CardActionArea>
+          <input accept="image/*" onChange={handleFileUpload} type="file" id="product-image" style={{ display: "none" }} />
+          <div style={{ textAlign: "center" }}>
+            <label htmlFor="product-image">
+              <Button variant="raised" component="span">
+                Upload
+              </Button>
+            </label>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClick}>Cancel</Button>
+          <Button onClick={addNewProduct}>Add</Button>
+        </DialogActions>
+      </Dialog>
+      <div style={{ marginTop: "10px", textAlign: "center" }}>
+        <Button variant="outlined" onClick={handleClick}>
+          Add new item
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default AddItemModal;
--- eshop-ui/src/pages/Products.js
+++ eshop-ui/src/pages/Products.js
@@ -1,54 +1,54 @@
 import * as React from "react";
-import {Button, CircularProgress, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
-import {useNavigate} from "react-router-dom"
+import { Button, CircularProgress, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import AddItemModal from "../components/dashboard/AddItemModal";
-import {useProducts} from "../api/productsApi";
+import { useProducts } from "../api/productsApi";
 
 const Products = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const { isLoading, products } = useProducts();
 
-    const {isLoading, products} = useProducts()
+  const loadingElement = isLoading && (
+    <TableRow>
+      <TableCell colSpan={5} align="center">
+        <CircularProgress />
+      </TableCell>
+    </TableRow>
+  ); // = false | <TableRow...>
 
-    const loadingElement = isLoading && (
-        <TableRow>
-            <TableCell colSpan={5} align="center"><CircularProgress /></TableCell>
-        </TableRow>
-    ) // = false | <TableRow...>
-
-    return (
-        <>
-            <Table size="small">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Pavadinimas</TableCell>
-                        <TableCell>Kaina</TableCell>
-                        <TableCell>Aprašymas</TableCell>
-                        <TableCell>Įkėlimo data</TableCell>
-                        <TableCell></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        loadingElement || products.map((listProduct, i) =>
-                            <TableRow key={i}>
-                                <TableCell>{listProduct.name}</TableCell>
-                                <TableCell>{listProduct.price}</TableCell>
-                                <TableCell>{listProduct.description}</TableCell>
-                                <TableCell></TableCell>
-                                <TableCell>
-                                    <Button variant="contained" onClick={() => navigate(`/products/${listProduct.id}`) }>
-                                        Preview
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        )
-                    }
-                </TableBody>
-            </Table>
-            <AddItemModal />
-        </>
-    )
-}
+  return (
+    <>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Pavadinimas</TableCell>
+            <TableCell>Kaina</TableCell>
+            <TableCell>Aprašymas</TableCell>
+            <TableCell>Įkėlimo data</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {loadingElement ||
+            products.map((listProduct, i) => (
+              <TableRow key={i}>
+                <TableCell>{listProduct.name}</TableCell>
+                <TableCell>{listProduct.price}</TableCell>
+                <TableCell>{listProduct.description}</TableCell>
+                <TableCell>{listProduct.createDate}</TableCell>
+                <TableCell>
+                  <Button variant="contained" onClick={() => navigate(`/products/${listProduct.id}`)}>
+                    Preview
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+      <AddItemModal />
+    </>
+  );
+};
 
 export default Products;
