a0a4f7eda06ca49281412a7d2efbec7a64654869
Tadas Simonavičius <tadas@inventi.lt>
2023-03-02 19:27:11 +0200

    16: refactor signup form with Formik

--- eshop-ui/src/pages/Signup.js
+++ eshop-ui/src/pages/Signup.js
@@ -1,51 +1,58 @@
-import {useState} from "react";
 import {Button, TextField} from "@mui/material";
 import Box from "@mui/material/Box";
+import {Field, Form, Formik} from "formik";
+import * as React from "react";
 
-const Signup = () => {
-
-    const [product, setProduct] = useState({
-        email: '',
-        password: '',
-        repeatedPassword: ''
-    })
-
-    return (
-        <>
-            <Box
-                component="form"
-                noValidate
-                autoComplete="off"
-                sx={{
-                    width: 300
+const Signup = () => (
+    <>
+        <Box
+            noValidate
+            autoComplete="off"
+            sx={{
+                width: 300
+            }}
+        >
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: '',
+                    repeatedPassword: ''
                 }}
-            >
-                <TextField
-                    id="email"
-                    label="Email"
-                    variant="standard"
-                    fullWidth
-                />
-                <TextField
-                    id="password"
-                    label="Password"
-                    type="password"
-                    variant="standard"
-                    fullWidth
-                />
-                <TextField
-                    id="repeatedPassword"
-                    label="Repeated Password"
-                    type="password"
-                    variant="standard"
-                    fullWidth
-                />
-                <Button sx={{
-                    marginTop: 2
-                }} variant="contained">Submit</Button>
-            </Box>
-        </>
-    )
-}
+                onSubmit={(values) => {
+                    console.log(values)
+                }}>
+                <Form>
+                    <Field id="email"
+                           name="email"
+                           label="Email"
+                           variant="standard"
+                           fullWidth
+                           as={TextField}
+                    />
+                    <Field id="password"
+                           name="password"
+                           label="Password"
+                           type="password"
+                           variant="standard"
+                           fullWidth
+                           as={TextField}
+                    />
+                    <Field id="repeatedPassword"
+                           name="repeatedPassword"
+                           label="Repeated Password"
+                           type="password"
+                           variant="standard"
+                           fullWidth
+                           as={TextField}
+                    />
+                    <Button type="submit" sx={{
+                        marginTop: 2
+                    }} variant="contained">Submit</Button>
+                </Form>
+            </Formik>
+        </Box>
+    </>
+)
+
 
 export default Signup
