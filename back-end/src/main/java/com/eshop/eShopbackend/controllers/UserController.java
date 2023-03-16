1b8c28f46154ae93c15942472447330ace05f644
Tadas Simonavičius <tadas@inventi.lt>
2023-03-16 19:25:44 +0200

    23: hard code UserService implementation as authenticated user

--- back-end/src/main/java/com/eshop/eShopbackend/controllers/UserController.java
+++ back-end/src/main/java/com/eshop/eShopbackend/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.eshop.eShopbackend.controllers;
 import com.eshop.eShopbackend.controllers.dto.LoginRequest;
 import com.eshop.eShopbackend.controllers.dto.LoginResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +23,10 @@ public class UserController {
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Valid LoginRequest loginRequest) {
 
-        authenticationManager.authenticate(null);
-        System.out.println(loginRequest);
+        Authentication authenticatedUser = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        System.out.println(authenticatedUser);
         return null;
     }
 }
--- back-end/src/main/java/com/eshop/eShopbackend/services/UserService.java
+++ back-end/src/main/java/com/eshop/eShopbackend/services/UserService.java
@@ -1,14 +1,52 @@
 package com.eshop.eShopbackend.services;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 @Service
 public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return null;
+            }
+
+            @Override
+            public String getPassword() {
+                return "{noop}password";
+            }
+
+            @Override
+            public String getUsername() {
+                return null;
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return true;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+        };
     }
 }
