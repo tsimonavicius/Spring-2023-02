2f6fdb3208de96ad30f0589e9c48d8b1a98e3bbe
Tadas Simonavičius <tadas@inventi.lt>
2023-03-16 21:06:58 +0200

    23: add default encoding for passwords hardcoded in DB

--- back-end/src/main/resources/data.sql
+++ back-end/src/main/resources/data.sql
@@ -8,10 +8,10 @@ VALUES ('Pirmojo produkto pavadinimas', 'neblogas produktas', 12, DEFAULT),
 INSERT INTO eshop_user (id, name, surname, email, password)
 VALUES
     -- user@email.com:user -- USER
-    (1, 'Vaflis', 'Kebabas', 'user@email.com', 'user'),
+    (1, 'Vaflis', 'Kebabas', 'user@email.com', '{noop}user'),
 
     -- admin@admin.com:admin -- USER, ADMIN
-    (2, 'Adminas', 'Adminiauskas', 'admin@admin.com', 'admin');
+    (2, 'Adminas', 'Adminiauskas', 'admin@admin.com', '{noop}admin');
 
 INSERT INTO role (name)
 VALUES
