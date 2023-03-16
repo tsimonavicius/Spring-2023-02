2a5fdcb7d60b65a08fd682b7c5d94187e502eefb
Tadas Simonavičius <tadas@inventi.lt>
2023-03-16 18:53:56 +0200

    23: create dummy AuthenticationManager with UserDetailsService

--- back-end/src/main/java/com/eshop/eShopbackend/configuration/SecurityConfig.java
+++ back-end/src/main/java/com/eshop/eShopbackend/configuration/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.eshop.eShopbackend.configuration;
 import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -43,5 +45,10 @@ public class SecurityConfig {
         return web -> web
                 .ignoring()
                 .antMatchers(properties.getPath() + "/**");
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- back-end/src/main/java/com/eshop/eShopbackend/controllers/UserController.java
+++ back-end/src/main/java/com/eshop/eShopbackend/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.eshop.eShopbackend.controllers;
 
 import com.eshop.eShopbackend.controllers.dto.LoginRequest;
 import com.eshop.eShopbackend.controllers.dto.LoginResponse;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,16 @@ import javax.validation.Valid;
 @RestController
 public class UserController {
 
+    private final AuthenticationManager authenticationManager;
+
+    public UserController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Valid LoginRequest loginRequest) {
+
+        authenticationManager.authenticate(null);
         System.out.println(loginRequest);
         return null;
     }
