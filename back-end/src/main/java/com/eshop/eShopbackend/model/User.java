c56d944077187c356aba8bbce63713fcb0072f3c
Tadas Simonavičius <tadas@inventi.lt>
2023-03-16 21:01:03 +0200

    23: create default users

--- back-end/src/main/java/com/eshop/eShopbackend/model/User.java
+++ back-end/src/main/java/com/eshop/eShopbackend/model/User.java
@@ -17,6 +17,7 @@ public class User {
     private Long id;
     private String name;
     private String surname;
+    private String email;
     private String password;
 
     @ManyToMany
--- back-end/src/main/resources/data.sql
+++ back-end/src/main/resources/data.sql
@@ -3,3 +3,23 @@ VALUES ('Pirmojo produkto pavadinimas', 'neblogas produktas', 12, DEFAULT),
        ('Antrojo produkto pavadinimas', 'neblogas produktas', 54, DEFAULT),
        ('Treciojo produkto pavadinimas', 'neblogas produktas', 77, DEFAULT),
        ('Ketvirtojo produkto pavadinimas', 'neblogas produktas', 32, DEFAULT);
+
+
+INSERT INTO eshop_user (id, name, surname, email, password)
+VALUES
+    -- user@email.com:user -- USER
+    (1, 'Vaflis', 'Kebabas', 'user@email.com', 'user'),
+
+    -- admin@admin.com:admin -- USER, ADMIN
+    (2, 'Adminas', 'Adminiauskas', 'admin@admin.com', 'admin');
+
+INSERT INTO role (name)
+VALUES
+    ('USER'),
+    ('ADMIN');
+
+INSERT INTO eshop_user_roles (user_id, roles_name)
+VALUES
+    (1, 'USER'),
+    (2, 'USER'),
+    (2, 'ADMIN');
