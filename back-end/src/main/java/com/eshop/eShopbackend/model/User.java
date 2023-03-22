640368b3710f8dc5a02f812baabae32634ad4013
Tadas Simonavičius <tadas@inventi.lt>
2023-03-22 18:58:49 +0200

    25: create custom JPQL query to fetch user with roles

--- back-end/src/main/java/com/eshop/eShopbackend/model/User.java
+++ back-end/src/main/java/com/eshop/eShopbackend/model/User.java
@@ -23,7 +23,7 @@ public class User implements UserDetails {
     private String email;
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany
     private Set<Role> roles;
 
     @Override
--- back-end/src/main/java/com/eshop/eShopbackend/repositories/UserRepository.java
+++ back-end/src/main/java/com/eshop/eShopbackend/repositories/UserRepository.java
@@ -2,9 +2,13 @@ package com.eshop.eShopbackend.repositories;
 
 import com.eshop.eShopbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = :email")
+    Optional<User> findByEmail(@Param("email") String email);
 }
