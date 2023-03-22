899c7583c56ad54e6fb16b811401bfea066d54e7
Tadas Simonavičius <tadas@inventi.lt>
2023-03-22 18:40:13 +0200

    25: validation and default values for ConfigurationProperties

--- back-end/src/main/java/com/eshop/eShopbackend/configuration/JwtProperties.java
+++ back-end/src/main/java/com/eshop/eShopbackend/configuration/JwtProperties.java
@@ -2,11 +2,18 @@ package com.eshop.eShopbackend.configuration;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
+@Validated
 @ConfigurationProperties(prefix = "eshop.jwt")
 public class JwtProperties {
-    private long validTimeInSeconds;
+
+    private long validTimeInSeconds = 1800;
+
+    @NotBlank
     private String secretKey;
 
     public long getValidTimeInMillis() {
