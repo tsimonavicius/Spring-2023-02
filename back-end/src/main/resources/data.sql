33506d2fe3a464adc6cd26ab4696a2a7b5248767
Tadas Simonavičius <tadas@inventi.lt>
2023-03-23 22:13:07 +0200

    25: add create timestamp for initial products

--- back-end/src/main/resources/data.sql
+++ back-end/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO PRODUCT (NAME, DESCRIPTION, PRICE, CREATE_DATE)
-VALUES ('Pirmojo produkto pavadinimas', 'neblogas produktas', 12, DEFAULT),
-       ('Antrojo produkto pavadinimas', 'neblogas produktas', 54, DEFAULT),
-       ('Treciojo produkto pavadinimas', 'neblogas produktas', 77, DEFAULT),
-       ('Ketvirtojo produkto pavadinimas', 'neblogas produktas', 32, DEFAULT);
+VALUES ('Pirmojo produkto pavadinimas', 'neblogas produktas', 12, now()),
+       ('Antrojo produkto pavadinimas', 'neblogas produktas', 54, now()),
+       ('Treciojo produkto pavadinimas', 'neblogas produktas', 77, now()),
+       ('Ketvirtojo produkto pavadinimas', 'neblogas produktas', 32, now());
 
 
 INSERT INTO eshop_user (id, name, surname, email, password)
